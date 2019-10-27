71e4ee78c8642ff4f18a3c59e15b089f6546aba9
stharonish <stharonish@gmail.com>
2019-10-27 22:15:45 +0545

    Article - update operation complete

--- app.js
+++ app.js
@@ -28,10 +28,13 @@ app.set('view engine', 'pug');
 
 // Body Parser Middleware
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 // parse application/json
 app.use(bodyParser.json());
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Home Route
 app.get('/', (req, res) => {
@@ -71,7 +74,45 @@ app.post('/article/add', (req, res) => {
     });
 });
 
+// Get Single Article
+app.get('/article/:id', (req, res) => {
+    Article.findById(req.params.id, (err, article) => {
+        res.render('article', {
+            article: article
+        });
+    });
+});
+
+// Load Edit Form
+app.get('/article/edit/:id', (req, res) => {
+    Article.findById(req.params.id, (err, article) => {
+        res.render('edit_article', {
+            title: 'Edit Article',
+            article: article
+        });
+    });
+});
+
+// Update Submit Post Route
+app.post('/article/edit/:id', (req, res) => {
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    let query = {_id: req.params.id};
+
+    Article.update(query, article, err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
 // Start Server
 app.listen(3000, () => {
-   console.log('Server started on port 3000...');
+    console.log('Server started on port 3000...');
 });
