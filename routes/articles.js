5735607d1af7bf2fa7b5fbb9647a45ebda170ca9
stharonish <stharonish@gmail.com>
2019-11-03 16:09:29 +0545

    Access control added

--- routes/articles.js
+++ routes/articles.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-// Bring in Article Model
+// Article Model
 let Article = require('../models/article');
+// User model
+let User = require('../models/user');
 
 // Add Route
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('add_article', {
         title: 'Add Article'
     })
@@ -14,7 +16,7 @@ router.get('/add', (req, res) => {
 // Add Submit POST route
 router.post('/add', (req, res) => {
     req.checkBody('title', 'Title is required').notEmpty();
-    req.checkBody('author', 'Author is required').notEmpty();
+    // req.checkBody('author', 'Author is required').notEmpty();
     req.checkBody('body', 'Body is requird').notEmpty();
 
     // Get Errors
@@ -28,7 +30,8 @@ router.post('/add', (req, res) => {
     } else {
         let article = new Article();
         article.title = req.body.title;
-        article.author = req.body.author;
+        // article.author = req.body.author;
+        article.author = req.user._id;
         article.body = req.body.body;
 
         article.save(err => {
@@ -36,7 +39,7 @@ router.post('/add', (req, res) => {
                 console.log(err);
                 return;
             } else {
-                req.flash('success', 'Articles Added');
+                req.flash('success', 'Article Added');
                 res.redirect('/');
             }
         });
@@ -46,15 +49,22 @@ router.post('/add', (req, res) => {
 // Get Single Article
 router.get('/:id', (req, res) => {
     Article.findById(req.params.id, (err, article) => {
-        res.render('article', {
-            article: article
+        User.findById(article.author, (err, user) => {
+            res.render('article', {
+                article: article,
+                author: user.name
+            });
         });
     });
 });
 
 // Load Edit Form
-router.get('/edit/:id', (req, res) => {
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
     Article.findById(req.params.id, (err, article) => {
+        if (article.author != req.user._id) {
+            req.flash('danger', 'Not Authorized');
+            return res.redirect('/');
+        }
         res.render('edit_article', {
             title: 'Edit Article',
             article: article
@@ -101,14 +111,34 @@ router.post('/edit/:id', (req, res) => {
 
 // Delete Article
 router.delete('/:id', (req, res) => {
+    if (!req.user._id) {
+        res.status(500).send();
+    }
+
     let query = {_id: req.params.id};
 
-    Article.remove(query, err => {
-        if (err) {
-            console.log(err);
+    Article.findById(req.params.id, (err, article) => {
+        if (article.author != req.user._id) {
+            res.status(500).send();
+        } else {
+            Article.remove(query, err => {
+                if (err) {
+                    console.log(err);
+                }
+                res.send('Success');
+            });
         }
-        res.send('Success');
     });
 });
+
+// Access Control
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        req.flash('danger', 'Please login');
+        res.redirect('/user/login');
+    }
+};
 
 module.exports = router;
